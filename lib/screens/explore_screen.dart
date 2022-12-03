df746c80705f5b4994f1008ad08714a588581e5f
thezx <thezxdev@gmail.com>
2022-12-03 16:36:32 -0700

    Buildindg today_recipe

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
 import '../models/explore_data.dart';
 
 class ExploreScreen extends StatelessWidget {
@@ -16,8 +17,8 @@ class ExploreScreen extends StatelessWidget {
       builder: (context, AsyncSnapshot<ExploreData> snapshot) {
         if( snapshot.connectionState == ConnectionState.done ) {
           final recipes = snapshot.data?.todayRecipes ?? [];
-          return const  Center(
-            child: Text('Show TodayRecipeListView'),
+          return TodayRecipeListView(
+            recipes: recipes
           );
         } else {
           return const Center(
