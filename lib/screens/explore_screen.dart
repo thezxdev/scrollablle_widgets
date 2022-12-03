6ec248fc32a5d7c12057adb8a248007d14c0f030
thezx <thezxdev@gmail.com>
2022-12-03 16:28:57 -0700

    exploreScreen uso del FutureBuilder

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../api/mock_fooderlich_service.dart';
+import '../models/explore_data.dart';
 
 class ExploreScreen extends StatelessWidget {
 
@@ -10,8 +11,20 @@ class ExploreScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Explore Screen'),
+    return FutureBuilder(
+      future: mockService.getExploreData(),
+      builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+        if( snapshot.connectionState == ConnectionState.done ) {
+          final recipes = snapshot.data?.todayRecipes ?? [];
+          return const  Center(
+            child: Text('Show TodayRecipeListView'),
+          );
+        } else {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      }
     );
   }
 }
