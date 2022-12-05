6be17fc26ae1860365a0864c2766af0e15748ffb
thezx <thezxdev@gmail.com>
2022-12-05 14:18:19 -0700

    Creado el recipes_grid_view (gridview  para mostrar las recetas)

---
+++ lib/components/recipes_grid_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+import 'components.dart';
+
+
+
+class RecipesGridView extends StatelessWidget {
+
+  final List<SimpleRecipe> recipes;
+
+  const RecipesGridView({
+    super.key,
+    required this.recipes
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(
+        left: 16,
+        right: 16,
+        top: 16
+      ),
+      child: GridView.builder(
+        itemCount: recipes.length,
+        gridDelegate: // Cuantas columnas
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (_, index) { // _ = context 
+          final simpleRecipe = recipes[index];
+          return RecipeThumbnail(
+            recipe: simpleRecipe
+          );
+        }
+      ),
+    );
+  }
+}
