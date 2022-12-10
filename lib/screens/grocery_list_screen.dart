f4a78c9b592100833ed649a1ce544176af491748
thezx <thezxdev@gmail.com>
2022-12-10 13:29:00 -0700

    grocery_list_screen

---
+++ lib/screens/grocery_list_screen.dart
@@ -0,0 +1,21 @@
+
+
+import 'package:flutter/material.dart';
+
+import '../models/models.dart';
+
+class GroceryListScreen extends StatelessWidget {
+
+  final GroceryManager manager;
+
+  const GroceryListScreen({
+    super.key,
+    required this.manager
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Repalce with ListView
+    return Container();
+  }
+}
